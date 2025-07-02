c89558573c8d1a5ec11fa0ff65bfdfea81f4d656
Farlon Morales <moralesaguiar@live.com>
2025-07-02 10:38:01 +0200

    Opp Game w2

--- js/main.js
+++ js/main.js
@@ -1,1 +1,42 @@
-console.log("New Game")
+
+const playerElm = document.getElementById(`player`);
+
+let positionY = 0; // vertical position 
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'p') {
+        console.log(`Move up`);
+        positionY += 10;
+        playerElm.style.bottom = positionY + 'px';
+    } else if (e.key === 'q') {
+        console.log('Move down');
+        positionY -= 10;
+        playerElm.style.bottom = positionY + 'px';
+
+    }
+});
+
+class Obstacle {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+        this.positionX = positionX; // 100
+        //this.newWave = newWave
+        //this.lionfish = lionFish
+
+        this.createDomElement();
+    }
+
+    createDomElement() {
+        const newWave = document.createElement("div");
+        //const lionFish = document.createElement("div");
+
+        newWave.className = "wave";
+        //lionFish.className = "lionFish";
+
+        const parentElm = document.getElementById("board");
+        parentElm.appendChild(newWave);
+        //parentElm.appendChild(lionFish);
+    }
+}
+
